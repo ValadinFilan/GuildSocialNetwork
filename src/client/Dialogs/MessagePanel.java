db3ff7b99774cce7e0198492cc28c1c0c8b413c2
lvd_2002 <lvd_2002@mail.ru>
2021-12-05 00:07:13 +0300

    Update

--- src/client/Dialogs/MessagePanel.java
+++ src/client/Dialogs/MessagePanel.java
@@ -1,10 +1,20 @@
 package client.Dialogs;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MessagePanel extends JPanel {
     JLabel text, time;
-    public MessagePanel(){
+    public MessagePanel(int x, int y, int width, int height, boolean right, String text, String time){
         super();
+        setLayout(new FlowLayout(right ? FlowLayout.RIGHT : FlowLayout.LEFT));
+        setBounds(x, y, width, height);
+        this.text = new JLabel(text);
+        this.time = new JLabel(time);
+        this.text.setFont(new Font("Courier", Font.BOLD,14));
+        this.time.setFont(new Font("Courier", Font.BOLD,8));
+        setBackground(Color.getHSBColor(.45F,.25f,.61f));
+        this.add(this.text);
+        this.add(this.time);
     }
 }
--- src/client/GUI.java
+++ src/client/GUI.java
@@ -2,6 +2,7 @@ package client;
 
 import client.Authorization.Authorization;
 import client.Authorization.Registration;
+import client.Dialogs.MessagePanel;
 
 import javax.swing.*;
 import java.awt.*;
@@ -10,14 +11,17 @@ public class GUI extends JFrame {
     public GUI(){
         super();
         setSize(400, 400);
-        setLayout(new FlowLayout());
-        Registration registration = new Registration(this, null);
-        Authorization authorization = new Authorization(this,registration);
-        registration.setAuthorization(authorization);
-        add(authorization);
-        add(registration);
+        setLayout(new FlowLayout(FlowLayout.CENTER));
+        //Registration registration = new Registration(this, null);
+        //Authorization authorization = new Authorization(this,registration);
+        //registration.setAuthorization(authorization);
+        //add(authorization);
+        //add(registration);
         setVisible(true);
-        authorization.setVisible(true);
+        //authorization.setVisible(true);
+        MessagePanel mp = new MessagePanel(0, 0, 400, 50, true, "Hello", "12:30");
+        mp.setVisible(true);
+        add(mp);
         setBackground(Color.getHSBColor(.45F,.25f,.61f));
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
