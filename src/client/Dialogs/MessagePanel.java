a5a6ebcf3586ea2d9555c8c24e5f365b9257a088
lvd_2002 <lvd_2002@mail.ru>
2021-12-06 01:11:49 +0300

    Update

--- src/client/Dialogs/MessagePanel.java
+++ src/client/Dialogs/MessagePanel.java
@@ -5,16 +5,22 @@ import java.awt.*;
 
 public class MessagePanel extends JPanel {
     JLabel text, time;
+    JPanel message;
     public MessagePanel(int x, int y, int width, int height, boolean right, String text, String time){
         super();
-        setLayout(new FlowLayout(right ? FlowLayout.RIGHT : FlowLayout.LEFT));
-        setBounds(x, y, width, height);
         this.text = new JLabel(text);
         this.time = new JLabel(time);
         this.text.setFont(new Font("Courier", Font.BOLD,14));
         this.time.setFont(new Font("Courier", Font.BOLD,8));
         setBackground(Color.getHSBColor(.45F,.25f,.61f));
-        this.add(this.text);
-        this.add(this.time);
+
+        setBounds(x, y, width, height);
+        message = new JPanel();
+        setLayout(new FlowLayout(right ? FlowLayout.RIGHT : FlowLayout.LEFT));
+
+        message.add(this.text);
+        message.add(this.time);
+        add(message);
+        setVisible(true);
     }
 }
--- src/client/GUI.java
+++ src/client/GUI.java
@@ -11,7 +11,16 @@ public class GUI extends JFrame {
     public GUI(){
         super();
         setSize(400, 400);
-        setLayout(new FlowLayout(FlowLayout.CENTER));
+        JPanel p = new JPanel();
+        BoxLayout b = new BoxLayout(p, BoxLayout.Y_AXIS);
+
+        p.setLayout(b);
+        setLayout(/*new FlowLayout()*/null);
+        p.setBounds(0, 0, 300, 300);
+        setLayout(new BorderLayout());
+
+
+
         //Registration registration = new Registration(this, null);
         //Authorization authorization = new Authorization(this,registration);
         //registration.setAuthorization(authorization);
@@ -19,9 +28,18 @@ public class GUI extends JFrame {
         //add(registration);
         setVisible(true);
         //authorization.setVisible(true);
-        MessagePanel mp = new MessagePanel(0, 0, 400, 50, true, "Hello", "12:30");
-        mp.setVisible(true);
-        add(mp);
+        for (int i = 0; i < 100; i++){
+            MessagePanel mp = new MessagePanel(0, 0, 300, 50, i % 2 == 0, "Hello" + i, "12:30");
+            p.add(mp, BorderLayout.CENTER);
+        }
+
+        JScrollPane pane = new JScrollPane(p);
+        pane.setMaximumSize(new Dimension(300, 200));
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        add(pane, BoxLayout.X_AXIS);
+        this.revalidate();
+
+
         setBackground(Color.getHSBColor(.45F,.25f,.61f));
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
